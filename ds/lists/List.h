b52674a98e93597b4ebc6a4e283e22870ccff849
2393494020 <2393494020@qq.com>
2019-05-08 17:52:07 +0800

    list

--- ds/lists/List.h
+++ ds/lists/List.h
@@ -18,26 +18,96 @@ private:
         Node *next;
 
         Node( const T & newData = T {}, Node * p = nullptr, Node * n = nullptr ) : data { newData }, prev { p }, next { n }
-        {
-
-        }
+        {}
 
         Node( T && newData, Node * p = nullptr, Node * n = nullptr ) : data { std::move(newData) }, prev { p }, next { n }
-        {
-
-        }
+        {}
     };
 
 
 public:
     class const_iterator
     {
+        public:
+            const_iterator() : current { nullptr }
+            {}
 
+            const T & operator* () const
+            {
+                return retrieve();
+            }
+
+            const_iterator & operator++ ()
+            {
+                current = current->next;
+                return *this;
+            }
+
+            const_iterator operator++ ( int )
+            {
+                const_iterator old = *this;
+                ++( *this );
+                return old;
+            }
+
+            bool operator== ( const const_iterator & rhs ) const
+            {
+                return current == rhs.current;
+            }
+
+            bool operator!= ( const const_iterator & rhs ) const
+            {
+                return !( *this == rhs );
+            }
+
+        protected:
+            Node *current;
+
+            T & retrieve() const
+            {
+                return current->data;
+            }
+
+            const_iterator(Node * p) : current { p }
+            {}
+
+            friend class List<T>;
     };
 
     class iterator : public const_iterator
     {
+        public:
+            iterator()
+            {}
 
+            T & operator* ()
+            {
+                return const_iterator::retrieve();
+            }
+
+            const T & operator*() const
+            {
+                return const_iterator::operator*();
+            }
+
+            iterator & operator++ ()
+            {
+                this->current = this->current->next;
+                return *this;
+            }
+
+            iterator operator++ ( int )
+            {
+                iterator old = *this;
+                ++( *this );
+                return old;
+            }
+
+        protected:
+            iterator( Node *p ) : const_iterator{ p }
+            {}
+
+            friend class List<T>;
     };
 
     explicit List<T>()
@@ -57,10 +127,13 @@ public:
     ~List<T>()
     {
         clear();
+        delete head;
+        delete tail;
     }
 
-    void clear() {
-
+    void clear()
+    {
+        
     }
 
     int size() const
@@ -130,23 +203,24 @@ public:
     void print() const
     {
         Node *p = head;
-        for (int i = 0; i < theSize; ++i)
+        for ( int i = 0; i < theSize; ++i )
         {
             p = p->next;
             std::cout << p->data << std::endl;
         }        
     }
     
-    void insert(int pos, const T & ele)
+    void insert( int pos, const T & ele )
     {
         Node *p = head;
-        for (int i = 0; i <= pos; ++i)
+        for ( int i = 0; i <= pos; ++i )
         {
             p = p->next;
         }
 
-        Node *current = new Node(ele, p->prev, p);
-        p->prev->next = current;
+        Node *newNode = new Node{ ele, p->prev, p };
+        p->prev->next = newNode;
+        p->prev = newNode;
 
         ++theSize;
     }
@@ -178,28 +252,39 @@ private:
 template<class T>
 List<T>::List( const List<T> & rhs )
 {
-    
+    List<T>::init();
+
+    for ( auto & ele : rhs )
+        List<T>::push_back( ele );
 }
 
 // copy assignment
 template<class T>
 List<T> & List<T>::operator= ( const List<T> & rhs )
 {
-
+    List<T> copy = rhs;
+    std::swap( *this, copy );
+    return *this;
 }
 
 // move constructor
 template<class T>
-List<T>::List( List<T> && rhs )
+List<T>::List( List<T> && rhs ) : theSize { rhs.theSize }, head { rhs.head }, tail { rhs.tail }
 {
-
+    rhs.theSize = 0;
+    rhs.head = nullptr;
+    rhs.tail = nullptr;
 }
 
 // copy assignment
 template<class T>
 List<T> & List<T>::operator= ( List<T> && rhs )
 {
+    std::swap( theSize, rhs.theSize );
+    std::swap( head, rhs.head );
+    std::swap( tail, rhs.tail );
 
+    return *this;
 }
 
 
