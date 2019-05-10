822f40c2e4057857b3c841a21b13674e408881a0
2393494020 <2393494020@qq.com>
2019-05-10 16:16:19 +0800

    tree

--- ds/queues/main.cpp
+++ ds/queues/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
 using namespace std;
-
+/**
+ * FILO
+ * Input a stack is by push; output is by pop and top.
+ * Only the top element is accessible.
+ **/
 int main(int argc, char const *argv[])
 {
     cout << "queues" << endl;
