dd25ea51b944ae5803c35d66dc821c9b5c682483
2393494020 <2393494020@qq.com>
2019-07-26 18:04:48 +0800

    quick

--- algorithm/src/Sort.cpp
+++ algorithm/src/Sort.cpp
@@ -3,8 +3,75 @@
 //
 
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
+
+void quickSort(int list[], int, int);
+void bubbleSort(int list[], int);
+
+int main() 
+{
+    int size = 100;
+    int list[size];
+    for (int i = 0; i < size; ++i) {
+        list[i] = i + 1;
+    }
+
+    for (int i = 1; i < size; ++i)
+    {
+        int j = rand() % i;
+        swap(list[i], list[j]);
+    }
+
+    quickSort(list, 0, size - 1);
+    // bubbleSort(list, size);
+
+    int *last = &list[size];
+    for (int *p = list; p < last; ++p) {
+        cout << *p << endl;
+    }
+
+    // cout << sizeof(list) << endl;
+    // cout << sizeof(list[0]) << endl;
+
+    // auto n = end(list) - begin(list);
+    // cout << "size:" << n << endl;
+
+    return 0;
+}
+
+void quickSort(int list[], int l, int h)
+{
+    if (l < h)
+    {
+        int x = list[l];
+        int i = l;
+        int j = h;
+
+        while (i < j)
+        {
+            while (j > i && list[j] > x)
+            {
+                --j;            
+            }
+            if (i < j)
+                list[i++] = list[j];
+
+            while (i < j && list[i] < x)
+            {
+                ++i;
+            }
+            if (i < j)
+                list[j--] = list[i];
+        }
+
+        list[i] = x;
+
+        quickSort(list, l, i - 1);
+        quickSort(&list[i + 1], i + 1, h);
+    }
+}
 
 void bubbleSort(int list[], int size)
 {
@@ -17,25 +84,4 @@ void bubbleSort(int list[], int size)
             }
         }
     }
-}
-
-int main() 
-{
-    int size = 10000;
-    int list[size] = {};
-    for (int i = 0; i < size; ++i) {
-        list[i] = i + 1;
-    }
-
-    bubbleSort(list, size);
-
-    int *last = &list[size];
-    for (int *p = list; p < last; ++p) {
-        cout << *p << endl;
-    }
-
-    // auto n = end(list) - begin(list);
-    // cout << "size:" << n << endl;
-
-    return 0;
 }
