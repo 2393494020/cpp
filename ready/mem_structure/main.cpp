82a53d3cee2838e65fbc9109ad5f7a48d85033fb
2393494020 <house.deng@qq.com>
2022-01-20 00:13:43 +0800

    Add files via upload

--- ready/mem_structure/main.cpp
+++ ready/mem_structure/main.cpp
@@ -9,7 +9,8 @@ int main()
 	int* p0 = &num;
 	int* p1 = new int(10);
 	char* p2 = new char[10];
-	strcpy_s(p2, 6, "hello");
+	// strcpy_s(p2, 6, "hello"); // for windows
+	strcpy(p2, "hello");
 
 	cout << *p1 << endl;
 	cout << p2 << endl;
