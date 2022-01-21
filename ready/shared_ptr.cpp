3072e1f6762bc5de42fa454ffd9fb257dbf43909
2393494020 <2393494020@qq.com>
2022-01-21 18:05:30 +0800

    Add files via upload

--- ready/shared_ptr.cpp
+++ ready/shared_ptr.cpp
@@ -8,11 +8,17 @@ int main()
 	shared_ptr<int> sp(new int(200));
 	cout << sp.use_count() << endl;
 	{
-		shared_ptr<int> spp = sp; // 引用计算+1
+		shared_ptr<int> spp = sp; // sp 控制权共享给了 spp, 引用计数+1
 		cout << *spp << endl;
 		cout << sp.use_count() << endl;
 		cout << spp.use_count() << endl;
 	} // 离开该区域, 引用计数-1
 	cout << sp.use_count() << endl;
+	cout << sp << endl;
+	shared_ptr<int> spp = move(sp); // sp 控制权转移给了 spp, 引用计数为 0
+	cout << spp << endl;
+	cout << (sp == nullptr ? -1 : *sp) << endl;
+	cout << "sp use count=" << sp.use_count() << endl;
+	cout << "spp use count=" << spp.use_count() << endl;
 	return 0;
 }
--- ready/weak_ptr.cpp
+++ ready/weak_ptr.cpp
@@ -1,9 +1,79 @@
 #include <iostream>
+#include <memory>
 
 using namespace std;
 
+struct B;
+struct A
+{
+	shared_ptr<B> pb;
+	~A()
+	{
+		cout << "~A" << endl;
+	}
+};
+
+struct B
+{
+	shared_ptr<A> pa;
+	~B()
+	{
+		cout << "~B" << endl;
+	}
+};
+
+
+struct WB;
+struct SA
+{
+	shared_ptr<WB> pb;
+	~SA()
+	{
+		cout << "~SA" << endl;
+	}
+};
+
+struct WB
+{
+	weak_ptr<SA> pa;
+	~WB()
+	{
+		cout << "~WB" << endl;
+	}
+};
+
 int main()
 {
-	cout << "Hello CMake." << endl;
+	// shared_ptr 相互引用无法释放
+	// weak_ptr 相当于观察者模式
+	{
+		shared_ptr<A> spa(new A);
+		shared_ptr<B> spb(new B);
+
+		cout << spa.use_count() << endl;
+		cout << spb.use_count() << endl;
+
+		spa->pb = spb;
+		spb->pa = spa;
+
+		cout << spa.use_count() << endl;
+		cout << spb.use_count() << endl;
+	} // 结束该域, 析构函数不被调用, 内存泄漏
+	
+	cout << "use weak_ptr" << endl;
+	{
+		shared_ptr<SA> spa(new SA);
+		shared_ptr<WB> wpb(new WB);
+
+		cout << spa.use_count() << endl;
+		cout << wpb.use_count() << endl;
+
+		spa->pb = wpb;
+		wpb->pa = spa;
+
+		cout << spa.use_count() << endl;
+		cout << wpb.use_count() << endl;
+	}
+	
 	return 0;
 }
