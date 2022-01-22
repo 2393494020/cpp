9221c9dda221861e8bc383a7e8843e43af321c32
2393494020 <2393494020@qq.com>
2022-01-22 18:40:02 +0800

    Add files via upload

--- ready/reference.cpp
+++ ready/reference.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 #include <memory>
+#include <assert.h>
 
 using namespace std;
+
+void swap_by_ref(int &a, int &b)
+{
+	int tmp = a;
+	a = b;
+	b = tmp;
+}
+
+void swap_by_ptr(int *a, int *b)
+{
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
 
 int main()
 {
@@ -16,5 +31,11 @@ int main()
 	cout << &ri << endl;
 	cout << sizeof(i) << endl;
 	cout << sizeof(ri) << endl;
+
+	int a = 12, b = 37;
+	swap_by_ref(a, b);
+	assert(a == 37 && b == 12);
+	swap_by_ptr(&a, &b);
+	assert(a == 12 && b == 37);
 	return 0;
 }
