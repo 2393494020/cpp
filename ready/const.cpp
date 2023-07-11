0fde319537dcf45c5ebd68da4e05f343902e1958
2393494020 <2393494020@qq.com>
2023-07-11 11:31:25 +0800

    Update const.cpp

--- ready/const.cpp
+++ ready/const.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 int main()
 {
-	// const 离'谁'近表示'谁'不可以修改
 	// const 修饰的对象先看左边，如果左边没有看右边
 	char str_hello[] = { "hello" };
 	char const* pstr1 = "world";           // 指针可以指向另一块地址但指针指向的内容不可修改, 等价于 const char* pstr1 = "world";
